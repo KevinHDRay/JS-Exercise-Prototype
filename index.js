b5adab26b3bdb2af8a9975fcb8bdddd802b72b22
Kevin Ray <kevinraytraveler@gmail.com>
2022-02-16 21:41:14 -0500

    Kevin Ray Sprint 2 Module 3 Project

--- index.js
+++ index.js
@@ -39,9 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name
+  this.age = age
+  this.stomach = []
 }
+
+  Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible)
+    }
+  }
+  Person.prototype.poop = function (){
+   this.stomach = []
+  }
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`
+  }
+  
+
 
 
 
@@ -63,8 +79,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model =  model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.odometer = 0
+  }
+  Car.prototype.fill = function(gallons){
+    this.tank = this.tank + gallons
+      }
+  Car.prototype.drive = function(distance){
+    const maxMiles = this.tank * this.milesPerGallon
+    if(distance <= maxMiles){
+      this.odometer = this.odometer + distance
+      this.tank = this.tank - (dist / milesPerGallon)
+    }else{
+    this.odometer = this.odometer + maxMiles
+    this.tank = 0
+    return `I ran out of fuel at ${this.odometer} miles`
+  }
 }
 
 
@@ -75,18 +108,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy 
+}
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global - The whole thing. The console itself with all the coding in it. It is almost always bad to bind here.
+  2. Implicit - The "this." syntax used when performing a function.
+  3. Explicit - Calling another object to be used.
+  4. New - Creates a new instance based on the constructor function.
 */
 
 
